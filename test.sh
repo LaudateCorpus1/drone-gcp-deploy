b065cb6b67bbaf03c25ffd9ca41220e579d38357
Glenn E. Bailey III <replicant@dallaslamers.org>
2020-01-17 14:56:49 -0600

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASE64_KEY=`cat unison-cloud-admin-base64`
-URL_MAP="adelphic-ui"
+BASE64_KEY=`cat keyfile`
+URL_MAP="my-website"
 
 docker run -e BASE64_KEY="$BASE64_KEY" -e URL_MAP="$URL_MAP" viant/drone-gcp-deploy
